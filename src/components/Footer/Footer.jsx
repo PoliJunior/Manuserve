8a4af74d462a37d6f89cbd288ffee6d1a31d34da
Henrique Pereira <henrique802011@gmail.com>
2022-02-03 15:16:30 -0300

    integracao com google maps feita

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,8 +1,19 @@
 import React from "react";
 import styles from "./Footer.module.css";
 import logoManuserve from "../../images/logo.png";
+import { GoogleMap, useJsApiLoader, Marker } from "@react-google-maps/api";
 
 const Footer = () => {
+  const { isLoaded } = useJsApiLoader({
+    id: "google-map-script",
+    googleMapsApiKey: "AIzaSyDpi11NxjaRnmU9VqB6AAp6_ldKi_Fik9Y",
+  });
+
+  const position = {
+    lat: -8.059528,
+    lng: -34.903736,
+  };
+
   return (
     <section className={styles.container}>
       <div className={styles.contato}>
@@ -15,7 +26,19 @@ const Footer = () => {
         <img src={logoManuserve} alt="Logo manuserve" />
       </div>
       <div className={styles.mapWrapper}>
-          <div className={styles.map}></div>
+        <div className={styles.map}>
+          {isLoaded ? (
+            <GoogleMap
+              mapContainerStyle={{ width: "100%", height: "100%" }}
+              center={position}
+              zoom={15}
+            >
+              <Marker position={position} />
+            </GoogleMap>
+          ) : (
+            <></>
+          )}
+        </div>
       </div>
     </section>
   );
