6ea3aa761f01cbf156b25d788f8999721fe3b2ea
Arthur Xavier <arthurcsxavier@gmail.com>
2022-08-27 12:10:43 -0300

    att favicon

--- src/components/InfoSection/InfoSection.jsx
+++ src/components/InfoSection/InfoSection.jsx
@@ -17,16 +17,16 @@ const InfoSection = () => {
           <h1>ManuServ</h1>
           <p>
             A empresa manuserv, criada no ano de 2008, focada no produto portas automáticas sociais, é
-            distribuidor da marca manusa, possui ampla gama de produtos e acessórios bem como
+            distribuidora da marca manusa. Possui ampla gama de produtos e acessórios, bem como
             soluções personalizadas para vários tipos de portas automáticas. Desde o início das suas
-            atividades vem desenvolvendo, capital humano qualificado, acervo técnico junto ao CREA,
+            atividades vem desenvolvendo capital humano qualificado, acervo técnico junto ao CREA,
             fornecedores confiáveis, infraestrutura logística, estoques e assistência técnica acompanhadas
             de ferramentas, instrumentos e materiais exclusivos, conferindo diagnósticos precisos aos
             nossos produtos. Grandes empresas de negócios e destaque expressivos do mercado, como
-            shoppings center, aeroportos, hospitais, construtoras, hotéis, resorts, condomínios,
+            shopping centers, aeroportos, hospitais, construtoras, hotéis, resorts, condomínios,
             supermercados, vem depositando grande confiança na nossa marca e assistência técnica.
             Escolher manuserv é marcar um encontro em algum lugar no futuro, é a escolha certa que
-            abri infinitas vezes a inteligente porta que nos aproxima.
+            abre infinitas vezes a inteligente porta que nos aproxima.
           </p>
           <br />
           <p>
