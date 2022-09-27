7f97c4e1427efd04e5959d897fe617215774a93b
Luana Rodrigues <marialuana2810@gmail.com>
2022-09-27 18:03:09 -0300

    comentando as redes sociais

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -24,10 +24,12 @@ const Footer = () => {
           <p>teste@gmail.com</p>
         </div>
         <div className={styles.vl}></div>
+      {/*
         <div id={styles.social}>
           <a href="https://www.facebook.com" className={styles.link} target="blank" rel="external"><i class="fa-brands fa-square-facebook"> /teste_face</i></a>
           <a href="https://www.instagram.com" className={styles.link} target="blank" rel="external"><i class="fa-brands fa-square-instagram"> @teste_insta</i></a>
         </div>
+      */}
       </div>
       <div className={styles.logo}>
         <img src={logoManuserve} alt="Logo manuserve" />
