f16214db487e617e2739af9124986a61fc57a9fa
Arthur Xavier <arthurcsxavier@gmail.com>
2022-10-18 12:06:49 -0300

    att footer redes sociais

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -23,13 +23,11 @@ const Footer = () => {
           <p>(81)9 3048-0206</p>
           <p>contato@manuservnordeste.com</p>
         </div>
-      {/*
         <div className={styles.vl}></div>
         <div id={styles.social}>
-          <a href="https://www.facebook.com" className={styles.link} target="blank" rel="external"><i class="fa-brands fa-square-facebook"> /teste_face</i></a>
-          <a href="https://www.instagram.com" className={styles.link} target="blank" rel="external"><i class="fa-brands fa-square-instagram"> @teste_insta</i></a>
+          <a href="https://contate.me/manuserv" className={styles.link} target="blank" rel="external"><i class="fa-brands fa-square-whatsapp"> WhatsApp</i></a>
+          <a href="https://www.instagram.com/manuservnordeste" className={styles.link} target="blank" rel="external"><i class="fa-brands fa-square-instagram"> @manuservnordest</i></a>
         </div>
-      */}
       </div>
       <div className={styles.logo}>
         <img src={logoManuserve} alt="Logo manuserve" />
