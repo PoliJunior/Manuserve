dd9ee599ffef407f53fc3895cc5fff85dbe49340
Arthur Xavier <arthurcsxavier@gmail.com>
2023-12-27 13:42:04 -0300

    Add Whats Btn

--- src/App.jsx
+++ src/App.jsx
@@ -8,10 +8,27 @@ import NavBar from "./components/Navbar";
 import Produtos from "./components/Produtos/Produtos";
 import Servicos from "./components/Servicos/Servicos";
 import Sobre from "./components/Sobre/Sobre";
+import img from "./images/whatsapp-icon.png"
+import notification from './videos/notification-sound.mp3'
+import React from 'react'
+import { FloatingWhatsApp } from 'react-floating-whatsapp';
 
 function App() {
   return (
     <div className="App">
+      <FloatingWhatsApp
+       phoneNumber="+5581995011503"
+       accountName="Manuserv" 
+       avatar={img} 
+       statusMessage="Geralmente responde em 24h" 
+       chatMessage={`Olá! 🤝\nComo podemos te ajudar?`} 
+       placeholder="Digite um texto..."
+       notification={true}
+       notificationDelay={30}
+       notificationSound={true}
+       notificationSoundSrc={notification}
+      />
+      {/* <button id="whats"> <img src={img} alt="WhatsApp Icon" /></button> */}
       <NavBar />
       <Home />
       <InfoSection />
